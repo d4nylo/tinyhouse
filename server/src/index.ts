90bca5f23030dcbd51af187954cc8192463b1d61
Danylo Kucherenko <danylo.kucherenko@gmail.com>
2022-02-10 16:52:34 +0000

    Added a basic GraphQL Schema

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,23 +1,18 @@
 import express from "express";
-import { listings } from "./listings";
+import { ApolloServer } from "apollo-server-express";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import { schema } from "./graphql";
 
 const app = express();
 const port = 9000;
 
-app.use(express.json());
+const server = new ApolloServer({
+  schema,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground],
+});
 
-app.get("/listings", (_req, res) => res.send(listings));
-
-app.post("/delete-listing", (req, res) => {
-  const id: string = req.body.id;
-
-  for (let i = 0; i < listings.length; i++) {
-    if (listings[i].id === id) {
-      return res.send(listings.splice(i, 1));
-    }
-  }
-
-  return res.send("failed to delete listing");
+server.start().then(() => {
+  server.applyMiddleware({ app, path: "/api" });
 });
 
 app.listen(port);
