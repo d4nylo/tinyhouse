17819f34c0e75dda83a3b5a8045e4b473bcec809
Danylo Kucherenko <danylo.kucherenko@gmail.com>
2022-02-13 16:32:35 +0000

    Added Styling with Ant Design

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import reportWebVitals from "./reportWebVitals";
 import { Listings } from "./sections";
+import "./styles/index.css";
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
