52d2364afe96e1dad836cacb28022dc88288ad1b
Danylo Kucherenko <danylo.kucherenko@gmail.com>
2022-02-14 21:05:31 +0000

    Building the UI for Login

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { Layout } from "antd";
 import reportWebVitals from "./reportWebVitals";
-import { Home, Host, Listing, Listings, NotFound, User } from "./sections";
+import { Home, Host, Listing, Listings, Login, NotFound, User } from "./sections";
 import "./styles/index.css";
 
 const client = new ApolloClient({
@@ -14,14 +15,17 @@ const client = new ApolloClient({
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/host" element={<Host />}></Route>
-        <Route path="/listing/:id" element={<Listing />}></Route>
-        <Route path="/listings/:location?" element={<Listings title="TinyHouse" />}></Route>
-        <Route path="/user/:id" element={<User />}></Route>
-        <Route path="*" element={<NotFound />}></Route>
-      </Routes>
+      <Layout id="app">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/host" element={<Host />}></Route>
+          <Route path="/listing/:id" element={<Listing />}></Route>
+          <Route path="/listings/:location?" element={<Listings title="TinyHouse" />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/user/:id" element={<User />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 };
