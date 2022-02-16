d221230e123315a4717db464aaea50c395cf2c52
Danylo Kucherenko <danylo.kucherenko@gmail.com>
2022-02-16 18:17:03 +0000

    Listing GraphQL Fields

--- server/src/graphql/resolvers/Listing/index.ts
+++ server/src/graphql/resolvers/Listing/index.ts
@@ -2,6 +2,11 @@ import { IResolvers } from "@graphql-tools/utils";
 import { Listing } from "../../../lib/types";
 
 export const listingResolvers: IResolvers = {
+  Query: {
+    listing: () => {
+      return "Query.listing";
+    },
+  },
   Listing: {
     id: (listing: Listing): string => {
       return listing._id.toString();
