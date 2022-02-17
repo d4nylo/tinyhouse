d3642054750ac7451d79a0e2dc203176025957da
Danylo Kucherenko <danylo.kucherenko@gmail.com>
2022-02-17 17:05:29 +0000

    Building the Listings Resolvers

--- server/src/graphql/typeDefs.ts
+++ server/src/graphql/typeDefs.ts
@@ -39,6 +39,11 @@ export const typeDefs = gql`
     result: [Listing!]!
   }
 
+  enum ListingsFilter {
+    PRICE_LOW_TO_HIGH
+    PRICE_HIGH_TO_LOW
+  }
+
   type User {
     id: ID!
     name: String!
@@ -66,7 +71,7 @@ export const typeDefs = gql`
     authUrl: String!
     user(id: ID!): User!
     listing(id: ID!): Listing!
-    listings: String!
+    listings(filter: ListingsFilter!, limit: Int!, page: Int!): Listings!
   }
 
   type Mutation {
